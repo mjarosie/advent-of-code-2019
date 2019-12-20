1e4e86b8871e68a610c43b85c7c23489b8b25aae
Maciej Jarosiewicz <mjarosie@users.noreply.github.com>
2019-12-20 21:12:43 +0000

    part one working

---
+++ day-7/part-one/app/Main.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified System.Environment as Env
+import qualified Debug.Trace        as Dbg
+import           Data.List (permutations)
+import           System.Process (
+                                  readCreateProcess
+                                , createProcess
+                                , proc
+                                , cwd
+                                )
+
+main :: IO ()
+main = do
+    [filename] <- Env.getArgs
+    results <- mapM (getThrustersOutputSignal filename 0) (permutations [0, 1, 2, 3, 4])
+    let siema = maximum results
+    print siema
+    -- r <- readCreateProcess (proc "stack" ["exec", "run-intcode-program", "../../day-7/input"]) { cwd = Just "../../day-5/part-two/" } "0\r\n0\r\n"
+    -- result <- getThrustersOutputSignal filename 0 [0, 1, 2, 3, 4] 
+    -- print $ result 
+
+getThrustersOutputSignal :: String -> Int -> [Int] -> IO Int
+getThrustersOutputSignal _ input [] = return input
+getThrustersOutputSignal filename input (phase:phases) =
+    do
+        let rawInput = show phase ++ "\r\n" ++ show input ++ "\r\n"
+        rawResponse <- readCreateProcess (proc "stack" ["exec", "run-intcode-program", "../../day-7/" ++ filename]) { cwd = Just "../../day-5/part-two/" } rawInput
+        let currentOutput = read rawResponse
+        getThrustersOutputSignal filename currentOutput phases
